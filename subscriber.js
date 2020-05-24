8ef14c7978f01d40a1377be1e16429766be1e37b
Salman Dabbakuti <dabbakutisalman@gmail.com>
2020-05-24 13:47:14 +0530

    Create subscriber.js

---
+++ subscriber.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const {PubSub} = require('@google-cloud/pubsub');
+const pubsub = new PubSub();
+let messageCount = 0;
+const messageHandler = message => {
+    console.log(`Received message ${message.id}:`);
+    console.log(`Data: ${message.data}`);
+    console.log(`tAttributes: ${message.attributes}`);
+    messageCount += 1;
+
+    // Ack the messae
+    message.ack();
+};
+
+pubsub.subscription('S123').on(`message`, messageHandler) //S123- Subscription name
+
+setTimeout(()=>{
+    pubsub.subscription('S123').removeListener('message', messageHandler);
+    console.log(`${messageCount} message(s) received.`);
+}, 5000);
