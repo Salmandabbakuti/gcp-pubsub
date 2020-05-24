bb0312aae0a000048753c9cf81f2ebb88278508e
Salman Dabbakuti <dabbakutisalman@gmail.com>
2020-05-24 13:57:17 +0530

    few fixes (#1)
    * Create package.json
    
    * Create subscriber.js
    
    * Rename publish.js to publisher.js
    
    * Create .env
    
    * Update .gitignore
    
    * Update publisher.js
    
    * Update publisher.js
    
    * Update publisher.js
    
    * Update subscriber.js

--- publisher.js
+++ publisher.js
@@ -1,16 +1,15 @@
-
 require('dotenv').config();
 const {PubSub} = require('@google-cloud/pubsub');
 const pubsub = new PubSub();
  async function publish() {
-    const data = new Date().toString();
+    const data = 'Hi Devs, I sent this message from Nodejs via GCP PubSub';
     const dataBuffer = Buffer.from(data);
     const topicName = 'pubsub';
     const messageId = await pubsub
         .topic(topicName)
         .publish(dataBuffer)
     
-    console.log(messageId)
+    console.log(`Message Published with and Id of ${messageId}`)
 }
 
 publish()
